f3e4962c56a39cf0cf04ad6bc865f88a6a6c643d
BuraYu <burak.yueksel@hotmail.com>
2024-10-09 20:42:07 +0200

    add AOVIncentive component

--- Projects/productDisplayPage/src/components/AOVIncentive/AOVIncetive.jsx
+++ Projects/productDisplayPage/src/components/AOVIncentive/AOVIncetive.jsx
@@ -1,10 +1,39 @@
 import "./aovIncetive.css";
 
 const AOVIncetive = () => {
+  let freeShipping = 200;
+  let freeShippingTarget = 200;
+  let missingAmount = freeShippingTarget - freeShipping;
+  let stylePercentage = (freeShipping / freeShippingTarget) * 100 > 20;
+  let minimumAmountReached = missingAmount <= 0;
+  let firstContainerWidth = (freeShipping / freeShippingTarget) * 100;
+  let secondContainerWidth = (missingAmount / freeShippingTarget) * 100;
+
   return (
-    <>
-      <h1 className="aov-incentive">AOVIncetive</h1>
-    </>
+    <div className="aov-container">
+      <div
+        className="incentive-container"
+        style={{ width: firstContainerWidth + "%" }}
+      >
+        {stylePercentage ? (
+          minimumAmountReached ? (
+            <span>Free shipping costs</span>
+          ) : (
+            <span>Only {missingAmount}$ away from free shipping</span>
+          )
+        ) : null}
+      </div>
+      {minimumAmountReached ? null : (
+        <div
+          className="incentive-missing"
+          style={{ width: secondContainerWidth + "%" }}
+        >
+          {stylePercentage ? null : (
+            <span>Only {missingAmount}$ away from free shipping</span>
+          )}
+        </div>
+      )}
+    </div>
   );
 };
 
