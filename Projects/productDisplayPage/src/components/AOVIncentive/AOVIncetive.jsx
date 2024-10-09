ef8446771f5443346ed925e5e7d085409d55a563
BuraYu <burak.yueksel@hotmail.com>
2024-10-09 21:44:11 +0200

    refactored the aovIncentive component

--- Projects/productDisplayPage/src/components/AOVIncentive/AOVIncetive.jsx
+++ Projects/productDisplayPage/src/components/AOVIncentive/AOVIncetive.jsx
@@ -1,36 +1,36 @@
 import "./aovIncetive.css";
 
 const AOVIncetive = () => {
-  let freeShipping = 200;
+  let freeShipping = 20;
   let freeShippingTarget = 200;
   let missingAmount = freeShippingTarget - freeShipping;
-  let stylePercentage = (freeShipping / freeShippingTarget) * 100 > 20;
-  let minimumAmountReached = missingAmount <= 0;
-  let firstContainerWidth = (freeShipping / freeShippingTarget) * 100;
-  let secondContainerWidth = (missingAmount / freeShippingTarget) * 100;
+  let percentageReached = (freeShipping / freeShippingTarget) * 100 > 20;
+  let hasReachedFreeShipping = missingAmount <= 0;
+  let firstContainerWidth = Math.max(freeShipping / freeShippingTarget) * 100;
+  let secondContainerWidth = Math.max(missingAmount / freeShippingTarget) * 100;
 
+  const renderMessage = () => {
+    if (hasReachedFreeShipping) {
+      return <span>Free shipping costs</span>;
+    }
+
+    return <span>Only {missingAmount}$ away from free shipping</span>;
+  };
   return (
     <div className="aov-container">
       <div
         className="incentive-container"
-        style={{ width: firstContainerWidth + "%" }}
+        style={{ width: `${firstContainerWidth}%` }}
       >
-        {stylePercentage ? (
-          minimumAmountReached ? (
-            <span>Free shipping costs</span>
-          ) : (
-            <span>Only {missingAmount}$ away from free shipping</span>
-          )
-        ) : null}
+        {percentageReached && renderMessage()}
       </div>
-      {minimumAmountReached ? null : (
+
+      {!hasReachedFreeShipping && (
         <div
           className="incentive-missing"
-          style={{ width: secondContainerWidth + "%" }}
+          style={{ width: `${secondContainerWidth}%` }}
         >
-          {stylePercentage ? null : (
-            <span>Only {missingAmount}$ away from free shipping</span>
-          )}
+          {!percentageReached && renderMessage()}
         </div>
       )}
     </div>
