b946a7f6780b1ba652718842d3a5e7b02cc82959
BuraYu <burak.yueksel@hotmail.com>
2024-10-11 22:49:31 +0200

    add aria-labels for payment methods, add lazy loading

--- Projects/productDisplayPage/src/components/Product/Product.jsx
+++ Projects/productDisplayPage/src/components/Product/Product.jsx
@@ -8,9 +8,12 @@ import paypal from "../../assets/payment/paypal.svg";
 import visa from "../../assets/payment/visa.svg";
 
 const Product = () => {
-  let home = "Home";
-  let category = "Category";
-  let productPage = "Product page";
+  const home = "Home";
+  const category = "Category";
+  const productPage = "Product page";
+
+  // const rating = 4;
+  // const totalStars = 5;
 
   return (
     <div className="product-wrapper">
@@ -37,19 +40,26 @@ const Product = () => {
 
         <div className="product__content-container">
           <div className="product__images">
-            <img src="https://picsum.photos/500" alt="A random 500px picture" />
+            <img
+              src="https://picsum.photos/500"
+              alt="A random 500px picture"
+              loading="lazy"
+            />
             <div className="product__thumbnail-container">
               <img
                 src="https://picsum.photos/150/115"
                 alt="a random 150x115 pictre"
+                loading="lazy"
               />
               <img
                 src="https://picsum.photos/150/115"
                 alt="a random 150x115 pictre"
+                loading="lazy"
               />
               <img
                 src="https://picsum.photos/150/115"
                 alt="a random 150x115 pictre"
+                loading="lazy"
               />
             </div>
           </div>
@@ -57,6 +67,7 @@ const Product = () => {
             <h1>Product name</h1>
             <div className="product__reviews">
               {/* Change to numbers at top, to iterate over them */}
+
               <img src={fullStar} alt="icon of a filled star" />
               <img src={fullStar} alt="icon of a filled star" />
               <img src={fullStar} alt="icon of a filled star" />
@@ -103,10 +114,26 @@ const Product = () => {
               <span>Checkout safety</span>
               <div className="line"></div>
               <div className="product__payment-methods">
-                <img src={paypal} alt="paypal logo" />
-                <img src={visa} alt="visa logo" />
-                <img src={mastercard} alt="mastercard logo" />
-                <img src={amex} alt="amex logo" />
+                <img
+                  src={paypal}
+                  alt="paypal logo"
+                  aria-label="PayPal Payment Methode"
+                />
+                <img
+                  src={visa}
+                  alt="visa logo"
+                  aria-label="Visa Payment Methode"
+                />
+                <img
+                  src={mastercard}
+                  alt="mastercard logo"
+                  aria-label="Visa Payment Methode"
+                />
+                <img
+                  src={amex}
+                  alt="amex logo"
+                  aria-label="Amex Payment Methode"
+                />
               </div>
             </div>
           </div>
