1f703cd760c4da228383db7b73b5135474bfcf32
BuraYu <burak.yueksel@hotmail.com>
2024-10-13 20:09:26 +0200

    add ProductInfo component

--- Projects/productDisplayPage/src/components/ProductInfo/ProductInfo.jsx
+++ Projects/productDisplayPage/src/components/ProductInfo/ProductInfo.jsx
@@ -3,7 +3,25 @@ import "./productInfo.css";
 const ProductInfo = () => {
   return (
     <>
-      <h1 className="product-info">ProductInfo</h1>
+      <div className="product-info__wrapper">
+        <div className="product-info__links">
+          <ul>
+            <li>
+              <a href="#">Description</a>
+            </li>
+            <li>
+              <a href="#">Composition & Care</a>
+            </li>
+            <li>
+              <a href="#">Shipping & Returns</a>
+            </li>
+            <li>
+              <a href="#">Free Samples</a>
+            </li>
+          </ul>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci sequi commodi dolores hic vel cupiditate dicta doloribus minus similique ratione quibusdam pariatur eligendi, ipsa eveniet culpa. Id, ut perferendis, repellat quam, quae eligendi minus quis recusandae sint provident perspiciatis laudantium veritatis quos itaque hic animi aliquid illo ipsam. Odio, aspernatur.</p>
+      </div>
     </>
   );
 };
