7b449f5be656bfb0be4cc6294a88765b2b12cbaa
BuraYu <burak.yueksel@hotmail.com>
2025-02-27 20:32:38 +0100

    refactored main content into main component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 
 import SidebarToggle from "./components/SidebarToggle";
-import Modal from "./components/Modal";
 import projects from "./data/data.js";
-
-// import berlinBear from "./images/gave_me_the_looks_3.png";
-import { SocialIcon } from "react-social-icons";
-import underConstruction from "./images/under_construction.svg";
-import glowingArrow from "./images/return.png";
-
-import reactIcon from "./icons/react-svgrepo-com.svg";
-import javascriptIcon from "./icons/javascript-svgrepo-com.svg";
-import typescriptIcon from "./icons/typescript-svgrepo-com.svg";
-import nodeIcon from "./icons/node-16-svgrepo-com.svg";
-import sassIcon from "./icons/sass-svgrepo-com.svg";
-import mongoIcon from "./icons/mongodb-svgrepo-com.svg";
-import nextJsIcon from "./icons/nextjs-fill-svgrepo-com.svg";
+import Main from "./components/Main.jsx";
 
 const App = () => {
   const [isBackendOpen, setIsBackendOpen] = useState(false);
@@ -107,114 +94,7 @@ const App = () => {
             ))}
           </Routes>
           {/* Portfolio Page Content */}
-          <div className="main-content-wrapper">
-            <img
-              src={underConstruction}
-              alt=""
-              className="under-construction"
-            />
-            {toggleSidebar ? null : (
-              <img src={glowingArrow} alt="" className="arrow" />
-            )}
-
-            <div class="parent">
-              <div class="div1 bento-box">
-                <div className="my-name-is-wrapper">
-                  <h2>Hello</h2>
-                  <h3>my name is</h3>
-                </div>
-                <div className="name_container">
-                  <h1>Burak</h1>
-                </div>
-              </div>
-              <div class="div2 bento-box">
-                <div className="chat-container">
-                  <div className="super-chat">
-                    <p>
-                      <span>BerlinDieter: </span>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Similique, sint obcaecati. Reprehenderit ipsa enim
-                      doloribus a facilis. Dolores, libero consectetur.
-                    </p>
-                  </div>
-                  <div className="super-chat">
-                    <p>
-                      <span>BerlinDieter: </span>
-                      ldfasdfasdfdf
-                    </p>
-                  </div>
-                  <div className="super-chat">
-                    <p>
-                      <span>BerlinDieter: </span>
-                      ldfasdfasdfdf
-                    </p>
-                  </div>
-                </div>
-                <div className="input-container">
-                  <input type="text" placeholder="Type a message..." />
-                  <button>Send</button>
-                </div>
-              </div>
-              <div class="div3 bento-box">
-                <h2 className="bento-header">socials.</h2>
-                <div className="social-links-container">
-                  <SocialIcon
-                    className="custom-class"
-                    url="http://www.github.com/burayu"
-                  />
-                  <SocialIcon url="https://www.linkedin.com/in/buraky%C3%BCksel-/" />
-                  <SocialIcon url="http://www.xing.de/burayu" />
-                  <SocialIcon url="mailto:burak.yueksel@hotmail.com" />
-                </div>
-              </div>
-              <div class="div4 bento-box">
-                <h2 className="bento-header">techstack.</h2>
-
-                <div className="icon-container-wrapper">
-                  <div className="icon-container">
-                    <img src={reactIcon} alt="react icon" />
-                    <p>React</p>
-                  </div>
-                  <div className="icon-container">
-                    <img src={javascriptIcon} alt="react icon" />
-                    <p>JavaScript</p>
-                  </div>
-                  <div className="icon-container">
-                    <img src={typescriptIcon} alt="react icon" />
-                    <p>Typescript</p>
-                  </div>
-                  <div className="icon-container">
-                    <img src={nextJsIcon} alt="react icon" />
-                    <p>NextJS</p>
-                  </div>
-                  <div className="icon-container">
-                    <img src={mongoIcon} alt="react icon" />
-                    <p>MongoDB</p>
-                  </div>
-                  <div className="icon-container">
-                    <img src={sassIcon} alt="react icon" />
-                    <p>Sass/SCSS</p>
-                  </div>
-                  <div className="icon-container">
-                    <img src={nodeIcon} alt="react icon" />
-                    <p>NodeJS</p>
-                  </div>
-                </div>
-              </div>
-              <div class="div5 bento-box">
-                <h2 className="bento-header">Who am i.</h2>
-                <p>Fullstack Dev based in Berlin</p>
-                {/* <img src={berlinBear} alt="Logo: Berlin Bear" /> */}
-              </div>
-              <div class="div6 bento-box">
-                <h2 className="bento-header">current project.</h2>
-                <Modal />
-              </div>
-              <div class="div7 bento-box">
-                <h2 className="bento-header">Add what here?</h2>
-              </div>
-            </div>
-          </div>
+          <Main toggleSidebar={toggleSidebar} />
         </div>
         {/* Sidebar Toggle Icon */}
         <SidebarToggle
